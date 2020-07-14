03d3e1213cff9fab589a61c9b012c0067c87d5e6
Mysh3ll <peacefulloijkn@gmail.com>
2020-07-14 20:36:09 +0200

    Testing failed validations

--- __tests__/integration/auth/register.spec.js
+++ __tests__/integration/auth/register.spec.js
@@ -10,21 +10,37 @@ const app = () => supertest(server);
 
 describe('The register process', () => {
 
+    const REGISTER_ENDPOINT ='/api/v1/auth/register';
+
+    const user = {
+        name: 'Test user',
+        email: 'test@user.com',
+        password: 'password'
+    };
+
     beforeEach(async () => {
        await User.deleteMany({});
     });
 
     it('should register a new user', async () => {
-        const response = await app().post('/api/v1/auth/register').send({
-            name: 'Test user',
-            email: 'test@user.com',
-            password: 'password'
-        });
+        const response = await app().post(REGISTER_ENDPOINT).send(user);
 
         expect(response.status).toBe(200);
         expect(response.body.data.token).toBeDefined();
         expect(response.body.message).toBe('Account registered.');
 
+    });
+
+    it('should return a 422 if registration fails', async () => {
+        await User.create(user);
+
+        const response = await app().post(REGISTER_ENDPOINT).send(user);
+
+        expect(response.status).toBe(422);
+        expect(response.body.message).toBe('Validation failed.');
+        expect(response.body.data.errors).toEqual({
+            email: 'This email has already been taken.'
+        });
     });
 
     afterAll( async () => {
